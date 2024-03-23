258272a3e042d37a5dbae29053e461f4880f65fb
Saeed-Moradi-1998 <saeed.moradi.edu@gmail.com>
2024-03-23 15:51:14 -0500

    Red. The third test of method named indexOfString has failed.

--- src/test/java/MyStringTest.java
+++ src/test/java/MyStringTest.java
@@ -12,6 +12,7 @@ public class MyStringTest {
         assertEquals(-1, MyString.indexOfString("world", "", 0));
         // This now passes in case s1 is empty.
         assertEquals(-1, MyString.indexOfString("", "world", 0));
-
+        // This should fail as the method does not handle this case.
+        assertEquals(6, MyString.indexOfString("hello world", "world", 0));
     }
 }
