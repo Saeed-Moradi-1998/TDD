aae07724064b437bf20108e54e53005e6f0fdbd1
Saeed-Moradi-1998 <saeed.moradi.edu@gmail.com>
2024-03-23 19:34:07 -0500

    Refactor. The condition at which either of the input strings in the method replace could be null is merged.

--- src/main/java/org/example/MyString.java
+++ src/main/java/org/example/MyString.java
@@ -30,15 +30,11 @@ public class MyString {
 
     public static String replace(String s, String s1, String s2) {
 
-        if (s == null){ // Checking if s in null.
+        // Check if any of the strings are null
+        if (s == null || s1 == null || s2 == null) {
             return null;
         }
-        if (s1 == null){ // Checking if s1 in null.
-            return null;
-        }
-        if (s2 == null){ // Checking if s2 is null
-            return null;
-        }
+
         return ""; // This is the initial implementation of the method which returns empty string.
     }
 }
